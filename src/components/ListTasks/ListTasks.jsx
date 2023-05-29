3c60c66ac8e165e4c8f01174845ee57ff0d44a38
Artem <artembariga5@gmail.com>
2023-05-29 14:53:58 +0300

    Fix

--- src/components/ListTasks/ListTasks.jsx
+++ src/components/ListTasks/ListTasks.jsx
@@ -1,37 +1,37 @@
-import { ItemTask } from "components/ItemTask/ItemTask";
-import React, { Component } from "react";
-import css from './ListTasks.module.scss'
+import { ItemTask } from 'components/ItemTask/ItemTask';
+import React, { Component } from 'react';
+import css from './ListTasks.module.scss';
 
 export class ListTasks extends Component {
-    handleCheckboxChange = (id) => {
-        const { tasks, onCheckboxChange } = this.props;
-        const updatedTasks = tasks.map((task) =>
-          task.id === id ? { ...task, completed: !task.completed } : task
-        );
-        onCheckboxChange(updatedTasks);
-    };
+  handleCheckboxChange = id => {
+    const { tasks, onCheckboxChange } = this.props;
+    const updatedTasks = tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    onCheckboxChange(updatedTasks);
+  };
 
-    handleDeleteTask = (id) => {
-        const { tasks, onCheckboxChange } = this.props;
-        const updatedTasks = tasks.filter((task) => task.id !== id);
+  handleDeleteTask = id => {
+    const { tasks, onCheckboxChange } = this.props;
+    const updatedTasks = tasks.filter(task => task.id !== id);
 
-        onCheckboxChange(updatedTasks);
-    }
+    onCheckboxChange(updatedTasks);
+  };
 
-    render () {
-        const { tasks } = this.props;
+  render() {
+    const { tasks } = this.props;
 
-        return (
-            <ul className={css.list}>
-                {tasks.map((task) => (
-                <ItemTask 
-                    tasks={task}
-                    key={task.id}
-                    onCheckboxChange={this.handleCheckboxChange}
-                    onDelete={this.handleDeleteTask}
-                    />
-                ))}
-            </ul>
-        );
-    }
+    return (
+      <ul className={css.list}>
+        {tasks.map(task => (
+          <ItemTask
+            tasks={task}
+            key={task.id}
+            onCheckboxChange={this.handleCheckboxChange}
+            onDelete={this.handleDeleteTask}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
